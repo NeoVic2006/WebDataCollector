0529cd2ce1432c3358d95ad7154d22e074ca55cb
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-30 14:58:01 -0600

    Database created

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Brithey912@localhost:7777/height_collector_web'
+db = SQLAlchemy(app)
 
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key = True)
+    email_ = db.Column(db.String(120), unique = True)
+    height_ = db.Column(db.Integer)
+
+    def __init__(self, email_, height_):
+        self.email_ = email_
+        self.height_ = height_
+        
+        
+        
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/success", methods=['POST'])
 def success_function():
     if request.method == 'POST':
         email = request.form["email_name_html"]
+        height = request.form["height_name_html"]
+        print(email, height)
     return render_template("success.html")
 
 
